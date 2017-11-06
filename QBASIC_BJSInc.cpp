cea4be3a0eaf5c92bfc6c0323556a0822b071817
Jerry Haron <12jh81@queensu.ca>
2017-11-06 15:14:58 -0500

    Removing write account functions
    Removing writeNewMasterAccountsFile and writeNewAccountsFile as they will not be used in assignment 3

--- QBASIC_BJSInc.cpp
+++ QBASIC_BJSInc.cpp
@@ -165,7 +165,7 @@ int main(){
 		}
 	}
     exit: //if user types Exit, the goto function leads back to here
-    writeNewMasterAccountsFile(AccountFile, validAccts);
+    //writeNewMasterAccountsFile(AccountFile, validAccts); FOR ASSIGNMENT 3 THIS IS REMOVED SINCE EACH TEST WILL BE USING THE EXACT SAME MASTER ACCOUNTS FILE
     writeNewAccountsFile(SummaryFile, validAccts);
     
     freopen("QBASIC_MergedTransactionSummaryFile.txt", "a+", stderr); //appends EOS to merged transaction summary log file
